b95074a7a4cb175b84cffd4a8ffd9d7f68a22bfd
Uzuki <246077556+HiroZukki@users.noreply.github.com>
2026-09-03 12:23:07 +0700

    vendorsetup: Use `--depth=1` for cloning dependencies
    Signed-off-by: Uzuki <246077556+HiroZukki@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,16 +1,16 @@
 echo "Cloning Stuffs Needed For Earth"
 
 # Other earth stuffs
-git clone https://github.com/HiroZukki/vendor_xiaomi_earth.git -b lineage-24.0 vendor/xiaomi/earth
+git clone https://github.com/HiroZukki/vendor_xiaomi_earth.git -b lineage-24.0 vendor/xiaomi/earth --depth=1
 git clone https://github.com/dreamsolister26/kernel_xiaomi_earth.git -b lineage-24.0 kernel/xiaomi/earth --depth=1
-git clone https://github.com/dreamsolister26/fortissimo.git -b evok vendor/evolution-priv/keys
+git clone https://github.com/dreamsolister26/fortissimo.git -b evok vendor/evolution-priv/keys --depth=1
 
 # Hardware Repos
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-24.0 hardware/mediatek
-git clone https://github.com/aobuta-prjkt/android_device_mediatek_sepolicy_vndr.git -b lineage-24.0 device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi --depth=1
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-24.0 hardware/mediatek --depth=1
+git clone https://github.com/aobuta-prjkt/android_device_mediatek_sepolicy_vndr.git -b lineage-24.0 device/mediatek/sepolicy_vndr --depth=1
 
 # Common IMS
-git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen-qpr2 vendor/mediatek/ims
+git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen-qpr2 vendor/mediatek/ims --depth=1
 
 echo "All Done!, Let's Start!"
