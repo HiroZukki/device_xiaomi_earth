8c5e1997fd0a2a34aa39acede762ddcc803f6304
Uzuki <246077556+HiroZukki@users.noreply.github.com>
2026-09-06 23:54:26 +0700

    earth: Implement vendorsetup.sh for cloning dependencies
    Signed-off-by: Uzuki <246077556+HiroZukki@users.noreply.github.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,13 @@
+1echo "Cloning Stuffs Needed For Earth"
+
+# Other earth stuffs
+git clone https://github.com/HiroZukki/vendor_xiaomi_earth.git -b 16.2-ims vendor/xiaomi/earth
+git clone https://github.com/dreamsolister26/kernel_xiaomi_earth.git -b lineage-23.2 kernel/xiaomi/earth
+git clone https://github.com/dreamsolister26/fortissimo.git -b keys vendor/lineage-priv/keys
+
+# Hardware Repos
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
+
+echo "All Done!, Let's Start!"
